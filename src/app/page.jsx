ba9819199e1bf6b8764555b21fb5cb03365d608a
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-05-28 04:06:58 +0600

    folder structure created

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <main className="flex items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold text-gradient">
+                DevMatch is coming.
+            </h1>
+        </main>
+    );
+}
